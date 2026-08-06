86724a311961bb8c21a8db221181debbc119edad
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2026-08-06 22:23:13 +0530

    Add payment webhook route and integrate payment routes in app

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,12 @@ app.use(cors({
     credentials: true
 }))
 
+// Register ONLY the webhook with raw body
+app.use(
+    "/api/v1/payment/webhook",
+    express.raw({ type: "application/json" })
+);
+
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({
     extended: true,
@@ -22,11 +28,13 @@ import userRoutes from "./routes/user.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
 import orderRoutes from "./routes/order.routes.js";
+import paymentRoutes from "./routes/payment.routes.js"
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/admin/product", productRoutes);
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/orders", orderRoutes);
+app.use("/api/v1/payments", paymentRoutes);
 
 export default app;
