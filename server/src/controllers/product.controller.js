6127b435953560f9c9ec745f125b5ae5eae9f0d9
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2026-08-26 16:14:46 +0530

    feat: Enhance updateProduct functionality to log parsed input data and update SKU for product sizes

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -220,7 +220,7 @@ const updateProduct = asyncHandler(async (req, res) => {
     const removedImages = JSON.parse(req.body.removedImages || "[]");
 
     const { name, description, originalPrice, discountPrice, stock, size, sku, hsn, tax, star, length, breadth, height, weight } = parsedInputData;
-    // console.log(parsedInputData);
+    console.log(parsedInputData);
 
     // console.log("removedImages", removedImages);
 
@@ -320,7 +320,8 @@ const updateProduct = asyncHandler(async (req, res) => {
                 { _id: productId, "sizes.size": size },
                 {
                     $set: {
-                        "sizes.$.stock": stock
+                        "sizes.$.stock": stock,
+                        "sizes.$.sku": sku
                     }
                 }
             )
