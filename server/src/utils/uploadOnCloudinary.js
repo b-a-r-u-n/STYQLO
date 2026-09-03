32e04041b156ab21e15f74db4f747de221a24c94
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2026-09-04 03:02:12 +0530

    fix: add logging for Cloudinary configuration details

--- server/src/utils/uploadOnCloudinary.js
+++ server/src/utils/uploadOnCloudinary.js
@@ -7,6 +7,8 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
+console.log("cloudinary config", process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_API_SECRET);
+
 const uploadOnCloudinary = async (fileLocalPaths) => {
     try {
         if(!fileLocalPaths || fileLocalPaths.length === 0)
