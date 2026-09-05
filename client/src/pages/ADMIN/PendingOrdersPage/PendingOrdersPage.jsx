ed4a46516c63f689d086768e32ac5a64cac0ba17
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2026-09-05 23:43:25 +0530

    fix: update shipmentId check logic in PendingOrdersPage

--- client/src/pages/ADMIN/PendingOrdersPage/PendingOrdersPage.jsx
+++ client/src/pages/ADMIN/PendingOrdersPage/PendingOrdersPage.jsx
@@ -57,10 +57,8 @@ const PendingOrdersPage = () => {
       // console.log(order);
 
       if (string === "accepted") {
-        console.log(order?.shiprocket?.shipmentId);
-        console.log(!order?.shiprocket?.shipmentId);
         
-        if(!order?.shiprocket?.shipmentId)
+        if(order?.shiprocket?.shipmentId === undefined)
           await createShiprocketOrder(orderId);
 
         const response = await getCourierDetails(orderId);
